e31cc9aee57feccdfb99ce0bbb44b80f0c3bf98c
END <vovk.fox@gmail.com>
2022-01-08 01:47:48 +0200

    Final stage of project, added cookie task saving.

--- To-Do List/task/src/main.js
+++ To-Do List/task/src/main.js
@@ -1,25 +1,21 @@
-
 //get all needed to add new tasks to list
-
 const add_task = document.getElementById('add-task-button');
 const task_list = document.getElementById('task-list');
 const task_input = document.getElementById('input-task');
 
-//checking if task_input is empty, if not enable button
-task_input.addEventListener('input', () => add_task.disabled = task_input.value.length < 0);
+let taskList = JSON.parse(localStorage.getItem("tasks")) || [];
 
-//adding new task to list
-add_task.addEventListener('click', () => {
+function addTask(text, enabled = false) {
     //create span
     const span = document.createElement('span')
     span.className = "task"
-    span.innerHTML = task_input.value
+    span.innerHTML = text
 
     //create input
     const input = document.createElement('input')
     input.type = "checkbox";
     input.className = "checkbox";
-
+    input.checked = enabled
     //create button
     const button = document.createElement('button')
     button.className = "delete-btn"
@@ -33,9 +29,32 @@ add_task.addEventListener('click', () => {
     li.append(label)
 
     task_list.append(li);
-    task_input.value = ''
-    add_task.disabled = true
+    input.addEventListener('change', function() {
+        taskList = taskList.map(u => u.text !== text ? u : {
+            enabled: this.checked,
+            text: text
+        });
+        localStorage.setItem("tasks", JSON.stringify(taskList))
+    })
+    button.addEventListener('click', () => {
+        taskList = taskList.filter(x => x.text !== span.innerHTML)
+        localStorage.setItem("tasks", JSON.stringify(taskList))
+        li.remove()
+    })
+}
 
-    button.addEventListener('click', () => li.remove())
+for (const task of taskList) {
+  addTask(task.text, task.enabled)
+}
+//checking if task_input is empty, if not enable button
+task_input.addEventListener('input', () => add_task.disabled = task_input.value.length < 0);
 
+//adding new task to list
+add_task.addEventListener('click', () => {
+    addTask(task_input.value)
+    taskList.push({
+        enabled: false,
+        text: task_input.value
+    })
+    localStorage.setItem("tasks", JSON.stringify(taskList))
 })
